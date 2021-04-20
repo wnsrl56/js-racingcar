4de1decf53fa1716ee003ffe24a203ad7a198636
jun <wnsrl56@gmail.com>
2021-04-20 22:06:54 +0900

    feat: racing module 추가

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+import Racing from './modules/Racing.js';
+
+export default function App() {
+  return new Racing();
+}
+
+App();
---
+++ src/js/modules/Racing.js
@@ -0,0 +1,7 @@
+export default function Racing() {
+  const init = () => {
+      
+  };
+
+  init();
+}
